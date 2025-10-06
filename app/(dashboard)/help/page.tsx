4e540de7dbdb87761b0c6ec30ea73cda4009c38c
arfatnoorisys <arfat.noorisys@gmail.com>
2025-10-06 17:54:03 +0530

    Other way to get help added

--- app/(dashboard)/help/page.tsx
+++ app/(dashboard)/help/page.tsx
@@ -1,5 +1,11 @@
 import { ContactSupportForm } from "@/components/contact-support-form";
+import { OtherWaysToGetHelp } from "@/components/other-way-get-help";
 
 export default function HelpPage() {
-  return <ContactSupportForm />;
+  return (
+    <>
+    <ContactSupportForm />
+    <OtherWaysToGetHelp />
+    </>
+  );
 }
