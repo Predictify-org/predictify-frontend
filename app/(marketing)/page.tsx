5e079a3dc13ed243f1868925f8012d4bd448d6c7
Jagadeeshftw <jagadeesh26062002@gmail.com>
2025-10-21 01:00:01 +0530

    chore: Remove marketing components to simplify codebase and improve maintainability

--- app/(marketing)/page.tsx
+++
@@ -1,9 +0,0 @@
-import MarketingContent from "./_components/marketing-content"
-
-export default function MarketingRoute() {
-  return (
-    <div className="min-h-screen bg-[#540D8D] relative overflow-hidden">
-      <MarketingContent />
-    </div>
-  )
-}
