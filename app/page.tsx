3f0e5adf3f82110528a04579b370dcf394139c44
Jagadeeshftw <jagadeesh26062002@gmail.com>
2025-10-21 01:18:05 +0530

    refactor: Simplify home page by removing unused AnimatedBackground component and adjusting imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,21 @@
 
 "use client"
 
-// import { CTA } from "@/components/sections/cta"
-import Features from "@/components/sections/features"
-import { Footer } from "@/components/sections/footer"
+import { Navbar } from "@/components/navbar/Navbar"
 import { Hero } from "@/components/sections/hero"
+import Features from "@/components/sections/features"
 import { HowItWorks } from "@/components/sections/how-it-work"
 import WalletsTokens from "@/components/sections/wallets-token"
-import { AnimatedBackground } from "@/components/ui/animated-background"
-import { Navbar } from "@/components/navbar/Navbar"
+import { Footer } from "@/components/sections/footer"
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-[#540D8D] relative overflow-hidden">
       <Navbar transparent />
-      <AnimatedBackground />
       <Hero />
       <Features />
       <HowItWorks />
       <WalletsTokens />
-      {/* <CTA/> */}
       <Footer />
     </div>
   )
