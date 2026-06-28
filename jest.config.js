8a088005f9395b4871ad4ec020e5175fe3ec9063
Awointa <akosawointa@gmail.com>
2026-06-28 06:46:18 +0100

    feat: add mock implementations for Next.js navigation and router

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,7 @@ const createJestConfig = nextJest({ dir: "./" });
 
 /** @type {import('jest').Config} */
 const config = {
-  // Use node environment for unit tests (config, lib, api)
-  // Component tests will run in a separate command with jsdom
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testMatch: ["**/*.test.{ts,tsx}", "**/*.spec.{ts,tsx}"],
   collectCoverageFrom: [
@@ -24,6 +22,8 @@ module.exports = async () => {
     ...resolvedConfig.moduleNameMapper,
     "^@/(.*)$": "<rootDir>/$1",
     "^\\./app/(.*)$": "<rootDir>/app/$1",
+    "^next/navigation$": "<rootDir>/__mocks__/next/navigation.js",
+    "^next/router$": "<rootDir>/__mocks__/next/router.js",
   };
   return resolvedConfig;
 };
